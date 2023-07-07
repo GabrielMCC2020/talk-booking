d8c6ad6b34b64951858a4f54075ae3ac788341c6
GabrielMCC2020 <gabrielmcc2002cultural@gmail.com>
2023-07-07 06:40:36 -0500

    Add-api

--- services/talk_booking/web_app/main.py
+++ services/talk_booking/web_app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .api_requests import SubmitTalkRequest
 
 app = FastAPI()
 
@@ -6,3 +7,18 @@ app = FastAPI()
 @app.get("/health-check/")
 def health_check():
     return {"message": "OK"}
+
+# new
+@app.post("/request-talk/", status_code=201)
+def request_talk(submit_talk_request: SubmitTalkRequest):
+    return {
+    "id": "unique_id",
+    "event_time": submit_talk_request.event_time,
+    "address": submit_talk_request.address,
+    "topic": submit_talk_request.topic,
+    "status": "PENDING",
+    "duration_in_minutes":
+submit_talk_request.duration_in_minutes,
+    "requester": submit_talk_request.requester,
+}
+    
